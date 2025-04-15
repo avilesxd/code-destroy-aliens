3d037ef28d8cf0b986bec71d06f09ea90d40ab89
Ignacio Avilés <nacho72001@gmail.com>
2025-04-15 16:43:00 -0400

    Refactor setup.py to dynamically load asset files
    - Updated setup.py to automatically gather image, music, and icon files from their respective directories.
    - Changed asset paths in DATA_FILES to reflect the new structure.
    - Corrected the application name from "Aliens Invasion" to "Alien Invasion" for consistency.

--- setup.py
+++ setup.py
@@ -6,35 +6,42 @@ Usage:
 """
 
 from setuptools import setup
+import os
 
 APP = ["main.py"]
 
+# Get the list of image files
+image_files = []
+for file in os.listdir("src/assets/images"):
+    if file.endswith((".png", ".bmp")):
+        image_files.append(os.path.join("src/assets/images", file))
+
+# Get the list of music files
+music_files = []
+for file in os.listdir("src/assets/music"):
+    if file.endswith(".mp3"):
+        music_files.append(os.path.join("src/assets/music", file))
+
+# Get the list of icon files
+icon_files = []
+for file in os.listdir("src/assets/icons"):
+    if file.endswith((".ico", ".png", ".icns")):
+        icon_files.append(os.path.join("src/assets/icons", file))
+
 DATA_FILES = [
-    (
-        "src/images",
-        [
-            "src/images/alien.png",
-            "src/images/F16.png",
-            "src/images/rocket.png",
-        ],
-    ),
-    (
-        "src/music",
-        [
-            "src/music/music.mp3",
-        ],
-    ),
-    ("src/icons", ["src/icons/icon.ico", "src/icons/icon.png"]),
+    ("src/assets/images", image_files),
+    ("src/assets/music", music_files),
+    ("src/assets/icons", icon_files),
 ]
 
 OPTIONS = {
     "argv_emulation": True,
     "includes": [],
-    "packages": ["src.configuration", "src.objects"],
-    "iconfile": "src/icons/icon-apple.icns",
+    "packages": ["src.config", "src.entities", "src.core"],
+    "iconfile": "src/assets/icons/icon-apple.icns",
     "plist": {
-        "CFBundleName": "Aliens Invasion",
-        "CFBundleDisplayName": "Aliens Invasion",
+        "CFBundleName": "Alien Invasion",
+        "CFBundleDisplayName": "Alien Invasion",
         "CFBundleIdentifier": "com.CodeWaveInnovation.DestroyAliens",
         "CFBundleVersion": "1.0.0",
         "CFBundleShortVersionString": "1.0",
@@ -43,7 +50,7 @@ OPTIONS = {
 
 setup(
     app=APP,
-    name="Aliens Invasion",
+    name="Alien Invasion",
     version="1.0.0",
     description="Space Invader 2D Game",
     author="Ignacio Avilés",
