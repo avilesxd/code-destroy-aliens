b410ff0b72e6664ba99754e1ef434125ae51367a
Ignacio Avilés <nacho72001@gmail.com>
2025-04-26 23:38:11 -0400

    chore: allow mixed case in commit message subject

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,7 +22,7 @@ module.exports = {
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [0, 'never'],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', 'lower-case'],
+    'subject-case': [0],
     'subject-empty': [0, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 150],
